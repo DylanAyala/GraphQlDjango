99df1773cea98695fd8c4eeb5dbc833c3e264abc
Dylan <dylan.ayala@sondeos.com.ar>
2019-04-27 13:59:49 -0300

    revoke refresh token

--- AuthGraph/schema.py
+++ AuthGraph/schema.py
@@ -10,9 +10,9 @@ class Query(usr.schema.Query, Links.schema.Query, graphene.ObjectType):
 
 class Mutation(usr.schema.Mutation, Links.schema.Mutation, graphene.ObjectType, ):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
-    #revoke_token = graphql_jwt.Revoke.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    revoke_token = graphql_jwt.Revoke.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- AuthGraph/settings.py
+++ AuthGraph/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Links',
     'usr',
     'corsheaders',
+    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
 ]
 
 MIDDLEWARE = [
